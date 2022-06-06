a51fae24dce52788d555041526765fc920a7e982
Ajand Mardalizad <aseedgeek@gmail.com>
2022-06-06 11:45:38 +0430

    base dashboard added

---
+++ src/Boilerplate.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+const Dashboard = () => {
+  return <div css={css`margin: 2em`}>This is the Dashboard</div>;
+};
+
+export default Dashboard;
--- src/apps/Router.js
+++ src/apps/Router.js
@@ -1,9 +1,11 @@
 import Header from "./common/Header";
+import Dashboard from "./governance/Dashboard";
 
 const Router = () => {
   return (
     <div>
       <Header />
+      <Dashboard />
     </div>
   );
 };
